86584687b8138aeaa3c59d41e78eee6fcca8ca68
Persephon <kam03m@gmail.com>
2013-12-24 14:29:31 -0500

    adding show more link functionality

--- js/directives/ItemList.js
+++ js/directives/ItemList.js
@@ -1,4 +1,4 @@
-storeApp.directive('itemList', function () {
+storeApp.directive('itemList', function ($timeout) {
     return {
         require: "^ngController",
         restrict: "A",
@@ -6,6 +6,9 @@ storeApp.directive('itemList', function () {
         link: function (scope, element, attr, controller) {
             scope.itemList = [];
             scope.loaderShown = true;
+
+            var detailsContainers = document.getElementsByClassName("details-container");
+            var itemContainers = document.getElementsByClassName("item-container");
 
             scope.showLoader = function () {
                 scope.loaderShown = true;
@@ -17,7 +20,29 @@ storeApp.directive('itemList', function () {
             };
 
             scope.refreshItems = function (items) {
+                _.forEach(items, function (item, i) {
+                    item.showShowMoreLink = false;
+                    item.detailsWidth = 370 - item.imageWidth;
+                });
+
                 scope.itemList = items;
+            };
+
+            scope.$watch('itemList', function (itemList) {
+                $timeout(function (){
+
+                    _.forEach(itemList, function (item, i) {
+                        if(detailsContainers[i].scrollHeight > 160) {
+                            item.showShowMoreLink = true;
+                        }
+                    });
+                });
+            });
+
+            scope.showMore = function (index) {
+                scope.itemList[index].showShowMoreLink = false;
+                itemContainers[index].className += " expanded";
+                console.log(index)
             };
 
             controller.setCartSiblingElement(element);
