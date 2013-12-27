59d58e63f8de9d16aeb1f3a827167c8d1f3509a7
Kyle Merritt <kam03m@gmail.com>
2013-12-27 10:51:54 -0500

    testing minified files on server

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,23 +6,53 @@ module.exports = function(grunt) {
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-        separator: ';'
+        mangle: {
+          except: ['$scope']
+        }
       },
       build: {
-        src: [
-          'js/app.js', 
-          'js/controllers/*.js', 
-          'js/directives/*.js',
-          'js/repositories/*.js'
-        ],
-        dest: 'js/build/storeApp.min.js'
+        files: {
+          'js/build/app.min.js': ['js/app.js'],
+          'js/build/MasterController.min.js': ['js/controllers/MasterController.js'],
+          'js/build/AutoSearchBox.min.js': ['js/directives/AutoSearchBox.js'],
+          'js/build/Cart.min.js': ['js/directives/Cart.js'],
+          'js/build/ItemList.min.js': ['js/directives/ItemList.js'],
+          'js/build/AWSEcommerce.min.js': ['js/repositories/AWSEcommerceViaProxy.js'],
+          'js/build/app.min.js': ['js/app.js'],
+        }
+        // src: [
+        //   'js/app.js', 
+        //   'js/repositories/AWSEcommerce.js',
+        //   'js/controllers/MasterController.js',
+        //   'js/directives/AutoSearchBox.js',
+        //   'js/directives/Cart.js',
+        //   'js/directives/ItemList.js'
+        // ],
+        // dest: 'js/build/storeApp.min.js'
       }
     },
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: [
+          'js/app.js', 
+          'js/build/AWSEcommerce.min.js',
+          'js/build/MasterController.min.js',
+          'js/build/AutoSearchBox.min.js',
+          'js/build/Cart.min.js',
+          'js/build/ItemList.min.js'
+        ],
+        dest: 'js/build/storeApp.min.js',
+      }
+    }
   });
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'concat']);
 
 };
